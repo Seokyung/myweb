8079dbd4c7953f93714be795a9feb2107c5800eb
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-04 23:57:06 +0900

    semantic ui react 추가
    package manager를 인스톨하는 방법으로 진행하려 했으나 각종 에러가 나서 CDN을 index.html 파일 안에(body에) 넣어주는 방식으로 대체했다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
-    <p>안녕하세요!</p>
+    <div>
+      <p> 안녕하세요</p>
+      <Button primary> 알아보기 </Button>
+      <Progress percent = {85} indicating />
+    </div>
   );
 }
 
