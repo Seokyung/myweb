b052033e0770f24a40886422b2f0694a45c773b8
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-05 20:49:20 +0900

    clonecoding_part5
    Grid를 이용해 이미지, 댓글 등을 가운데로 정렬시켰고 Divider를 이용해 구분선을 그어줌. 좋아요 버튼과 공유 버튼을 추가함.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import {Image} from 'semantic-ui-react'
-import Comments from './comments.js'
-import sunbaGuitar from "./sunbaGuitar.jpeg"
+import React from "react";
+import { Image, Divider, Header, Icon, Button, Grid } from "semantic-ui-react";
+import Comments from "./comments.js";
+import sunbaGuitar from "./img/sunbaGuitar.jpeg";
 
 function App() {
   return (
     <div>
-      <Image
-        src = {sunbaGuitar}
-        style={{width:'500px'}}
-        centered
-      />
-      <Comments/>
+      <Grid centered>
+        <Grid.Row>
+          <Image src={sunbaGuitar} style={{ width: "400px" }} centered />
+        </Grid.Row>
+        <Grid.Row>
+          <Button
+            color="red"
+            content="Like"
+            icon="heart"
+            label={{
+              basic: true,
+              color: "red",
+              pointing: "left",
+              content: "0",
+            }}
+          />
+          <Button
+            basic
+            color="blue"
+            content="Share"
+            icon="fork"
+            label={{
+              as: "a",
+              basic: true,
+              color: "blue",
+              pointing: "left",
+              content: "0",
+            }}
+          />
+        </Grid.Row>
+      </Grid>
+      <br />
+      <br />
+
+      <Comments />
+
+      <br />
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="share" />
+          Share With
+        </Header>
+      </Divider>
+      <br />
+
+      <div>
+        <Grid centered>
+          <Button color="facebook">
+            <Icon name="facebook" /> Facebook
+          </Button>
+          <Button color="twitter">
+            <Icon name="twitter" /> Twitter
+          </Button>
+          <Button color="google plus">
+            <Icon name="google" /> Google
+          </Button>
+          <Button color="instagram">
+            <Icon name="instagram" /> Instagram
+          </Button>
+          <Button color="youtube">
+            <Icon name="youtube" /> YouTube
+          </Button>
+        </Grid>
+      </div>
+      <br />
     </div>
   );
 }
--- src/comments.js
+++ src/comments.js
@@ -1,36 +1,58 @@
-import React from 'react';
-import {Comment, Form, Button, Header} from 'semantic-ui-react'
-import userIcon from "./user.png"
+import React from "react";
+import {
+  Grid,
+  Comment,
+  Form,
+  Button,
+  Divider,
+  Header,
+  Icon,
+} from "semantic-ui-react";
+import "./comments.css";
+import userIcon from "./img/user.png";
 
-class Comments extends React.Component{
+class Comments extends React.Component {
   render() {
-    return(
-      <Comment.Group>
-        <Header as='h3' dividing>
-          Comments
-        </Header>
+    return (
+      <Grid centered>
+        <div id="commentCenter">
+          <Comment.Group>
+            <Divider horizontal>
+              <Header as="h3">
+                <Icon name="comments" />
+                Comments
+              </Header>
+            </Divider>
 
-        <Comment>
-          <Comment.Avatar src={userIcon} />
-          <Comment.Content>
-            <Comment.Author as='a'>Matt</Comment.Author>
-            <Comment.Metadata>
-              <div>Today at 5:42PM</div>
-            </Comment.Metadata>
-            <Comment.Text>How artistic!</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-
-        <Form reply>
-          <Form.TextArea />
-          <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-        </Form>
-      </Comment.Group>
-    )
+            <Comment>
+              <Comment.Avatar src={userIcon} />
+              <Comment.Content>
+                <Comment.Author as="a">순돌</Comment.Author>
+                <Comment.Metadata>
+                  <div>오늘 5:42PM</div>
+                </Comment.Metadata>
+                <Comment.Text>레오 간지 개쩌러</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>답글 달기</Comment.Action>
+                </Comment.Actions>
+                <Form reply>
+                  <Form.TextArea
+                    style={{ width: "550px", minHeight: "100px" }}
+                  />
+                  <Button
+                    content="Add Reply"
+                    labelPosition="left"
+                    icon="edit"
+                    primary
+                  />
+                </Form>
+              </Comment.Content>
+            </Comment>
+          </Comment.Group>
+        </div>
+      </Grid>
+    );
   }
 }
 
-export default Comments
+export default Comments;
