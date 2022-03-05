69451a8e381eb55d01d4c89291b79d00f47f2b97
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-05 16:41:58 +0900

    clonecoding_part3
    class 컴포넌트와 이벤트창(alert)에 input값을 띄우는 법을 배움.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Button, Progress, Input} from 'semantic-ui-react'
 
-function Login() {
+//function으로 컴포넌트를 만드는 방식
+function Login2() {
   return(
     <div>
       <Input
@@ -10,9 +11,47 @@ function Login() {
         placeholder='아이디를 입력하세요.'
       />
       &nbsp;
-      <Button primary> 로그인 </Button>
+      <Button primary onClick = { (e) => {
+        console.log(e)
+        alert(e.target.value)
+      } }> 로그인 </Button>
     </div>
   );
+}
+
+//class로 컴포넌트를 만드는 방식
+class Login extends React.Component {
+  //Login state의 id인 값(입력값이 없음)
+  constructor() {
+    super()
+    this.state = {id : "입력값이 없음"}
+  }
+  render() {
+    return(
+      <div>
+        {/*Input의 value가 Login state에 새롭게 저장됨
+          (내가 쓴 내용이 alert 창에 띄워진다는 뜻)*/}
+        <Input
+          label={{ icon: 'asterisk' }}
+          labelPosition='left corner'
+          placeholder='아이디를 입력하세요.'
+          onChange = { (e) => this.setState({id : e.target.value}) }
+        />
+        &nbsp;      
+        <Button primary onClick = { (event) => {
+          alert(this.state.id)
+        } }> Login </Button>
+        {/*e는 event의 e
+        onClick이라는 이벤트가 발생한 target이 바로 Login Button
+        value는 this.state의 id(입력값이 없음) */}
+        {/*또는
+          <Button primary value = {this.state.id} onClick = { (event) => {
+            alert(event.target.value)
+          } }> Login </Button>
+        */}
+      </div>
+    )
+  }
 }
 
 function App() {
