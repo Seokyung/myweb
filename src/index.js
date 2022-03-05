06ee4a5dc873ca3ff6675b0ed58c0fed738bb0c9
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-05 20:55:16 +0900

    trimmed codes with prettier
    packages 중 prettier를 사용해 코드를 깔끔하게 정리해줌 (Ctrl + Alt + f)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App /> {/*App.js의 function App()을 불러옴*/}
   </React.StrictMode>,
-  document.getElementById('root') //pubic의 index.html 안에 있는 <body>태그 안의 <div>태그(id가 root인)를 가져옴
+  document.getElementById("root") //pubic의 index.html 안에 있는 <body>태그 안의 <div>태그(id가 root인)를 가져옴
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/login.js
+++ src/login.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import {Button, Input} from 'semantic-ui-react'
+import React from "react";
+import { Button, Input } from "semantic-ui-react";
 
 //function으로 컴포넌트를 만드는 방식
 function Login2() {
-  return(
+  return (
     <div>
       <Input
-        label={{ icon: 'asterisk' }}
-        labelPosition='left corner'
-        placeholder='아이디를 입력하세요.'
+        label={{ icon: "asterisk" }}
+        labelPosition="left corner"
+        placeholder="아이디를 입력하세요."
       />
       &nbsp;
-      <Button primary onClick = { (e) => {
-        console.log(e)
-        alert(e.target.value)
-      } }> 로그인 </Button>
+      <Button
+        primary
+        onClick={(e) => {
+          console.log(e);
+          alert(e.target.value);
+        }}
+      >
+        로그인
+      </Button>
     </div>
   );
 }
@@ -23,24 +28,29 @@ function Login2() {
 class Login extends React.Component {
   //Login state의 id인 값(입력값이 없음)
   constructor() {
-    super()
-    this.state = {id : "입력값이 없음"}
+    super();
+    this.state = { id: "입력값이 없음" };
   }
   render() {
-    return(
+    return (
       <div>
         {/*onChange : Input으로 받은 value가 Login state에 새롭게 저장됨
           (내가 쓴 내용이 alert 창에 띄워진다는 뜻)*/}
         <Input
-          label={{ icon: 'asterisk' }}
-          labelPosition='left corner'
-          placeholder='아이디를 입력하세요.'
-          onChange = { (e) => this.setState({id : e.target.value}) }
+          label={{ icon: "asterisk" }}
+          labelPosition="left corner"
+          placeholder="아이디를 입력하세요."
+          onChange={(e) => this.setState({ id: e.target.value })}
         />
         &nbsp;
-        <Button primary onClick = { (event) => {
-          alert(this.state.id)
-        } }> Login </Button>
+        <Button
+          primary
+          onClick={(event) => {
+            alert(this.state.id);
+          }}
+        >
+          Login
+        </Button>
         {/*e는 event의 e
         onClick이라는 이벤트가 발생한 target이 바로 Login Button
         value는 this.state의 id(입력값이 없음) */}
@@ -50,6 +60,8 @@ class Login extends React.Component {
           } }> Login </Button>
         */}
       </div>
-    )
+    );
   }
 }
+
+export default Login;
