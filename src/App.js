0a5a734317af1c8cb2b9634e5ae3eb3c9bd880a0
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-03-05 12:10:18 +0900

    clonecodeing_part2
    html, css, js, React의 구조와 semantic ui의 구체적인 사용법을 배움

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Button, Progress, Input} from 'semantic-ui-react'
+
+function Login() {
+  return(
+    <div>
+      <Input
+        label={{ icon: 'asterisk' }}
+        labelPosition='left corner'
+        placeholder='아이디를 입력하세요.'
+      />
+      &nbsp;
+      <Button primary> 로그인 </Button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <p> 안녕하세요</p>
+      <p>안녕하세요!</p>
+      <button> 버튼1 </button>
+      <Button color="red"> 버튼2 </Button>
       <Button primary> 알아보기 </Button>
       <Progress percent = {85} indicating />
+      <Login/> //fuction Login()을 불러옴
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> //App.js의 function App()을 불러옴
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') //pubic의 index.html 안에 있는 <body>태그 안의 <div>태그(id가 root인)를 가져옴
 );
 
 // If you want to start measuring performance in your app, pass a function
